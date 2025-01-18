f9cf6f90c9295079c811afc36d98f8669688f136
zhugechina <zhugetd@126.com>
2025-01-18 20:56:38 +0800

    add google ads

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -12,6 +12,8 @@ export default function App({ Component, pageProps }: AppProps): ReactElement {
   return (<>
     <Head>
       <meta name="saashub-verification" content="zm6rcf35ll2n" />
+      <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4907197973761221"
+        crossOrigin="anonymous"></script>
     </Head>
 
     {/* Google tag (gtag.js)  */}
