3a76b0b681d732e0bce94dfc23d36494e7e35616
zhugechina <zhugetd@126.com>
2025-02-04 11:44:33 +0800

    change sitemap

--- next-sitemap.config.mjs
+++ next-sitemap.config.mjs
@@ -2,7 +2,8 @@
 
 /** @type {import('next-sitemap').IConfig} */
 export default {
-  siteUrl: "https://ezymp3.cc",
+  // siteUrl: "https://ezymp3.cc",
+  siteUrl: "https://youtubetomp3-dwj.pages.dev",
   generateIndexSitemap: false,
   generateRobotsTxt: false,
   // output: "export", // Set static output here
